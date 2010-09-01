3f2a7198a1f0237c9cce8099c3b39f0e981f18ae
Alexander Bruy <alexander.bruy@gmail.com>
2010-09-01 17:13:14 +0000

    add translations

---
+++ i18n/photo2shape_ru_RU.ts
@@ -0,0 +1,159 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>Photo2Shape</name>
+    <message>
+        <location filename="photo2shape.py" line="74"/>
+        <source>Quantum GIS version detected: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape.py" line="75"/>
+        <source>This version of RasterCalc requires at least QGIS version 1.0.0
+Plugin will not be enabled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape.py" line="79"/>
+        <source>Create a point shapefile from a set of geotagged images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape.py" line="97"/>
+        <source>About Photo2Shape</source>
+        <comment>Window title</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape.py" line="99"/>
+        <source>&lt;b&gt;Photo2Shape&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape.py" line="102"/>
+        <source>Version: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape.py" line="105"/>
+        <source>This plugin creates a point shapefile
+from a set of geotagged images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape.py" line="106"/>
+        <source>&lt;b&gt;Developers:&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape.py" line="109"/>
+        <source>&lt;b&gt;Homepage:&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape.py" line="126"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Photo2ShapeDialog</name>
+    <message>
+        <location filename="photo2shapedialog.py" line="176"/>
+        <source>Photo2Shape</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialogbase.ui" line="22"/>
+        <source>Directory with images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialogbase.ui" line="55"/>
+        <source>Browse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialogbase.ui" line="43"/>
+        <source>Output shapefile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialogbase.ui" line="73"/>
+        <source>Add result to canvas</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="62"/>
+        <source>Select directory with images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="73"/>
+        <source>No images found</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="73"/>
+        <source>There are no supported images in this directory. Please select another one.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="87"/>
+        <source>Select output shapefile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="112"/>
+        <source>Delete error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="112"/>
+        <source>Can&apos;t delete file %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="125"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="139"/>
+        <source>There are no geotagged photos in selected directory.
+Shapefile was not created.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="166"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="176"/>
+        <source>Error loading output shapefile:
+%1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="187"/>
+        <source>QML exists</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="189"/>
+        <source>QML file %1 already exists. Overwrite?</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="196"/>
+        <source>QML error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="196"/>
+        <source>Can&apos;t write QML file</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
