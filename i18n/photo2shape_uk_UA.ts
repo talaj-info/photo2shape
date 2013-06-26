bf2479d73ab80e9497dfbbf886703f8cb08bc35c
Alexander Bruy <alexander.bruy@gmail.com>
2013-06-26 16:17:07 +0300

    strings update

--- i18n/photo2shape_uk_UA.ts
+++ i18n/photo2shape_uk_UA.ts
@@ -1,85 +1,90 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="1.1">
+<!DOCTYPE TS><TS version="2.0">
 <context>
     <name>Photo2Shape</name>
     <message>
-        <location filename="photo2shape.py" line="74"/>
+        <location filename="photo2shape.py" line="76"/>
         <source>Quantum GIS version detected: </source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shape.py" line="79"/>
+        <location filename="photo2shape.py" line="83"/>
         <source>Create a point shapefile from a set of geotagged images</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shape.py" line="97"/>
+        <location filename="photo2shape.py" line="104"/>
         <source>About Photo2Shape</source>
         <comment>Window title</comment>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shape.py" line="99"/>
+        <location filename="photo2shape.py" line="106"/>
         <source>&lt;b&gt;Photo2Shape&lt;/b&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shape.py" line="102"/>
-        <source>Version: %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="photo2shape.py" line="105"/>
+        <location filename="photo2shape.py" line="112"/>
         <source>This plugin creates a point shapefile
 from a set of geotagged images</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shape.py" line="106"/>
+        <location filename="photo2shape.py" line="113"/>
         <source>&lt;b&gt;Developers:&lt;/b&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shape.py" line="109"/>
+        <location filename="photo2shape.py" line="116"/>
         <source>&lt;b&gt;Homepage:&lt;/b&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shape.py" line="126"/>
+        <location filename="photo2shape.py" line="133"/>
         <source>Close</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shape.py" line="75"/>
-        <source>This version of Photo2Shape requires at least QGIS version 1.0.0
+        <location filename="photo2shape.py" line="76"/>
+        <source>This version of Photo2Shape requires at least QGIS version 2.0.
 Plugin will not be enabled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape.py" line="95"/>
+        <source>Photo2Shape</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape.py" line="109"/>
+        <source>Version: %s</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>Photo2ShapeDialog</name>
     <message>
-        <location filename="photo2shapedialog.py" line="183"/>
+        <location filename="photo2shapedialog.py" line="207"/>
         <source>Photo2Shape</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialogbase.ui" line="22"/>
+        <location filename="photo2shapedialogbase.ui" line="20"/>
         <source>Directory with images</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialogbase.ui" line="55"/>
+        <location filename="photo2shapedialogbase.ui" line="53"/>
         <source>Browse</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialogbase.ui" line="43"/>
+        <location filename="photo2shapedialogbase.ui" line="41"/>
         <source>Output shapefile</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialogbase.ui" line="73"/>
+        <location filename="photo2shapedialogbase.ui" line="69"/>
         <source>Add result to canvas</source>
         <translation type="unfinished"></translation>
     </message>
@@ -89,81 +94,91 @@ Plugin will not be enabled.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialog.py" line="73"/>
+        <location filename="photo2shapedialog.py" line="75"/>
         <source>No images found</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialog.py" line="73"/>
+        <location filename="photo2shapedialog.py" line="75"/>
         <source>There are no supported images in this directory. Please select another one.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialog.py" line="87"/>
+        <location filename="photo2shapedialog.py" line="92"/>
         <source>Select output shapefile</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialog.py" line="119"/>
+        <location filename="photo2shapedialog.py" line="130"/>
         <source>Delete error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialog.py" line="119"/>
-        <source>Can&apos;t delete file %1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="photo2shapedialog.py" line="132"/>
+        <location filename="photo2shapedialog.py" line="151"/>
         <source>Cancel</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialog.py" line="146"/>
+        <location filename="photo2shapedialog.py" line="167"/>
         <source>There are no geotagged photos in selected directory.
 Shapefile was not created.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialog.py" line="173"/>
+        <location filename="photo2shapedialog.py" line="197"/>
         <source>Close</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialog.py" line="183"/>
-        <source>Error loading output shapefile:
-%1</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="photo2shapedialog.py" line="194"/>
+        <location filename="photo2shapedialog.py" line="221"/>
         <source>QML exists</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialog.py" line="196"/>
-        <source>QML file %1 already exists. Overwrite?</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="photo2shapedialog.py" line="203"/>
+        <location filename="photo2shapedialog.py" line="231"/>
         <source>QML error</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialog.py" line="203"/>
+        <location filename="photo2shapedialog.py" line="231"/>
         <source>Can&apos;t write QML file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialog.py" line="111"/>
+        <location filename="photo2shapedialog.py" line="121"/>
         <source>Wrong output file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photo2shapedialog.py" line="112"/>
+        <location filename="photo2shapedialog.py" line="121"/>
         <source>Output file is improperly defined.
 Please enter a valid filename and try again.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="90"/>
+        <source>Shapefiles (*.shp *.SHP)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="130"/>
+        <source>Can&apos;t delete file %s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="175"/>
+        <source>The following files were not added to shapefile because of errors: &lt;br&gt;&lt;br&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="207"/>
+        <source>Error loading output shapefile:
+%s</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shapedialog.py" line="221"/>
+        <source>QML file %s already exists. Overwrite?</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
