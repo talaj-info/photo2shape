d07ed8bf28dcc5f7f9fa37fbb49f81311a2fcbe8
Alexander Bruy <alexander.bruy@gmail.com>
2014-11-20 16:12:38 +0200

    ukrainian translation update

--- i18n/photo2shape_uk.ts
+++ i18n/photo2shape_uk.ts
@@ -1,16 +1,17 @@
 <?xml version="1.0" encoding="utf-8"?>
-<!DOCTYPE TS><TS version="2.0">
+<!DOCTYPE TS>
+<TS version="2.0" language="uk_UA">
 <context>
     <name>AboutDialog</name>
     <message>
         <location filename="gui/aboutdialog.py" line="53"/>
         <source>Version: %s</source>
-        <translation type="unfinished"></translation>
+        <translation>Версія: %s</translation>
     </message>
     <message>
         <location filename="gui/aboutdialog.py" line="77"/>
         <source>&lt;p&gt;Create point shapefile from a set of geotagged photos. Inspired by ImagesToShape plugin from Tim Sutton.&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Developers&lt;/strong&gt;: Alexander Bruy&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;http://hub.qgis.org/projects/photo2shape&quot;&gt;http://hub.qgis.org/projects/photo2shape&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Please report bugs at &lt;a href=&quot;http://hub.qgis.org/projects/photo2shape/issues&quot;&gt;bugtracker&lt;/a&gt;.&lt;/p&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;p&gt;Створює точковий shape-файл з набору геотегованих фотографій. На розробку надихнув плагін ImagesToShape за авторством Tim Sutton.&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Розробники&lt;/strong&gt;: Alexander Bruy&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Домашня сторінка&lt;/strong&gt;: &lt;a href=&quot;http://hub.qgis.org/projects/photo2shape&quot;&gt;http://hub.qgis.org/projects/photo2shape&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Про помилки повідомляйте у &lt;a href=&quot;http://hub.qgis.org/projects/photo2shape/issues&quot;&gt;багтрекер&lt;/a&gt;.&lt;/p&gt;</translation>
     </message>
 </context>
 <context>
@@ -18,12 +19,12 @@
     <message>
         <location filename="ui/aboutdialogbase.ui" line="14"/>
         <source>About Photo2Shape</source>
-        <translation type="unfinished"></translation>
+        <translation>Про Photo2Shape</translation>
     </message>
     <message>
         <location filename="ui/aboutdialogbase.ui" line="44"/>
         <source>TextLabel</source>
-        <translation type="unfinished"></translation>
+        <translation>TextLabel</translation>
     </message>
     <message>
         <location filename="ui/aboutdialogbase.ui" line="30"/>
@@ -32,7 +33,11 @@
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:16pt; font-weight:600;&quot;&gt;Photo2Shape&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p align=&quot;center&quot; style=&quot; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;&quot;&gt;&lt;span style=&quot; font-size:16pt; font-weight:600;&quot;&gt;Photo2Shape&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="ui/aboutdialogbase.ui" line="67"/>
@@ -41,42 +46,46 @@ p, li { white-space: pre-wrap; }
 p, li { white-space: pre-wrap; }
 &lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
 &lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation type="unfinished"></translation>
+        <translation>&lt;!DOCTYPE HTML PUBLIC &quot;-//W3C//DTD HTML 4.0//EN&quot; &quot;http://www.w3.org/TR/REC-html40/strict.dtd&quot;&gt;
+&lt;html&gt;&lt;head&gt;&lt;meta name=&quot;qrichtext&quot; content=&quot;1&quot; /&gt;&lt;style type=&quot;text/css&quot;&gt;
+p, li { white-space: pre-wrap; }
+&lt;/style&gt;&lt;/head&gt;&lt;body style=&quot; font-family:&apos;Droid Sans&apos;; font-size:10pt; font-weight:400; font-style:normal;&quot;&gt;
+&lt;p style=&quot;-qt-paragraph-type:empty; margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px; font-family:&apos;Sans Serif&apos;; font-size:9pt;&quot;&gt;&lt;br /&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="ui/photo2shapedialogbase.ui" line="14"/>
         <source>Photo2Shape</source>
-        <translation type="unfinished"></translation>
+        <translation>Photo2Shape</translation>
     </message>
     <message>
         <location filename="ui/photo2shapedialogbase.ui" line="34"/>
         <source>Browse...</source>
-        <translation type="unfinished"></translation>
+        <translation>Огляд...</translation>
     </message>
     <message>
         <location filename="ui/photo2shapedialogbase.ui" line="27"/>
         <source>Output shapefile</source>
-        <translation type="unfinished"></translation>
+        <translation>Вихідний shape-файл</translation>
     </message>
     <message>
         <location filename="ui/photo2shapedialogbase.ui" line="51"/>
         <source>Directory with images</source>
-        <translation type="unfinished"></translation>
+        <translation>Каталог з фото</translation>
     </message>
     <message>
         <location filename="ui/photo2shapedialogbase.ui" line="61"/>
         <source>Add result to canvas</source>
-        <translation type="unfinished"></translation>
+        <translation>Завантажити результат у QGIS</translation>
     </message>
     <message>
         <location filename="ui/photo2shapedialogbase.ui" line="68"/>
         <source>Recurse subdirectories</source>
-        <translation type="unfinished"></translation>
+        <translation>Рекурсивний обхід каталогів</translation>
     </message>
     <message>
         <location filename="ui/photo2shapedialogbase.ui" line="75"/>
         <source>Append to existing file</source>
-        <translation type="unfinished"></translation>
+        <translation>Дописати в існуючий файл</translation>
     </message>
 </context>
 <context>
@@ -84,29 +93,31 @@ p, li { white-space: pre-wrap; }
     <message>
         <location filename="photo2shape_plugin.py" line="97"/>
         <source>Photo2Shape</source>
-        <translation type="unfinished"></translation>
+        <translation>Photo2Shape</translation>
     </message>
     <message>
         <location filename="photo2shape_plugin.py" line="67"/>
         <source>QGIS %s detected.
 This version of Photo2Shape requires at least QGIS 2.0.
 Plugin will not be enabled.</source>
-        <translation type="unfinished"></translation>
+        <translation>Знайдено QGIS %s.
+Ця версія Photo2Shape потребує QGIS 2.0 або новішу.
+Плагін не буде активовано.</translation>
     </message>
     <message>
         <location filename="photo2shape_plugin.py" line="76"/>
         <source>Create a point shapefile from geotagged images</source>
-        <translation type="unfinished"></translation>
+        <translation>Створити точковий shape-файл з набору геотегованих зображень</translation>
     </message>
     <message>
         <location filename="photo2shape_plugin.py" line="79"/>
         <source>About Photo2Shape...</source>
-        <translation type="unfinished"></translation>
+        <translation>Про Photo2Shape...</translation>
     </message>
     <message>
         <location filename="photo2shape_plugin.py" line="82"/>
         <source>About Photo2Shape</source>
-        <translation type="unfinished"></translation>
+        <translation>Про Photo2Shape</translation>
     </message>
 </context>
 <context>
@@ -114,47 +125,47 @@ Plugin will not be enabled.</source>
     <message>
         <location filename="gui/photo2shapedialog.py" line="97"/>
         <source>ESRI Shapefiles (*.shp *.SHP)</source>
-        <translation type="unfinished"></translation>
+        <translation>Shape-файли ESRI (*.shp *.SHP)</translation>
     </message>
     <message>
         <location filename="gui/photo2shapedialog.py" line="86"/>
         <source>Select directory</source>
-        <translation type="unfinished"></translation>
+        <translation>Виберіть каталог</translation>
     </message>
     <message>
         <location filename="gui/photo2shapedialog.py" line="100"/>
         <source>Save file</source>
-        <translation type="unfinished"></translation>
+        <translation>Зберегти файл</translation>
     </message>
     <message>
         <location filename="gui/photo2shapedialog.py" line="123"/>
         <source>Path not set</source>
-        <translation type="unfinished"></translation>
+        <translation>Шлях не задано</translation>
     </message>
     <message>
         <location filename="gui/photo2shapedialog.py" line="123"/>
         <source>Path to photos is not set. Please specify directory with photos and try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Каталог зображень не задано. Будь ласка задайте каталог зображень та спробуйте ще раз.</translation>
     </message>
     <message>
         <location filename="gui/photo2shapedialog.py" line="131"/>
         <source>Output file is not set</source>
-        <translation type="unfinished"></translation>
+        <translation>Вихідний файл не задано</translation>
     </message>
     <message>
         <location filename="gui/photo2shapedialog.py" line="131"/>
         <source>Output file name is missing. Please specify correct output file and try again.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вказано ім&apos;я вихідного файлу. Будь ласка вкажіть ім&apos;я вихідного файлу та спробуйте ще раз.</translation>
     </message>
     <message>
         <location filename="gui/photo2shapedialog.py" line="159"/>
         <source>Import completed</source>
-        <translation type="unfinished"></translation>
+        <translation>Імпорт завершено</translation>
     </message>
     <message>
         <location filename="gui/photo2shapedialog.py" line="173"/>
         <source>Cannot load output shapefile</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалось завантажити вихідний shape-файл</translation>
     </message>
 </context>
 <context>
@@ -162,22 +173,22 @@ Plugin will not be enabled.</source>
     <message>
         <location filename="photoimporter.py" line="69"/>
         <source>Unable to open or create layer.</source>
-        <translation type="unfinished"></translation>
+        <translation>Не вдалось створити або відкрити шар.</translation>
     </message>
     <message>
         <location filename="photoimporter.py" line="82"/>
         <source>No images found in directory.</source>
-        <translation type="unfinished"></translation>
+        <translation>У каталозі відсутні зображення.</translation>
     </message>
     <message>
         <location filename="photoimporter.py" line="93"/>
         <source>Skipping file %s: there are no GPS tags in it.</source>
-        <translation type="unfinished"></translation>
+        <translation>Файл %s пропущено: відсутні теги GPS.</translation>
     </message>
     <message>
         <location filename="photoimporter.py" line="101"/>
         <source>Skipping file %s: there are no GPS fix data.</source>
-        <translation type="unfinished"></translation>
+        <translation>Файл %s пропущено: відсутні координати GPS.</translation>
     </message>
 </context>
 </TS>
