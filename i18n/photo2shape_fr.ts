ea270360c22fff45f194362db470357010cab2af
Alexander Bruy <alexander.bruy@gmail.com>
2015-11-20 16:24:38 +0200

    strings update

--- i18n/photo2shape_fr.ts
+++ i18n/photo2shape_fr.ts
@@ -3,13 +3,13 @@
 <context>
     <name>AboutDialog</name>
     <message>
-        <location filename="gui/aboutdialog.py" line="53"/>
-        <source>Version: %s</source>
+        <location filename="gui/aboutdialog.py" line="80"/>
+        <source>&lt;p&gt;Create point shapefile from a set of geotagged photos. Inspired by ImagesToShape plugin from Tim Sutton.&lt;/p&gt;&lt;p&gt;NOTE: plugin requires 3rd party Python package exifread &gt;= 2.0.0&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Developers&lt;/strong&gt;: Alexander Bruy&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;http://hub.qgis.org/projects/photo2shape&quot;&gt;http://hub.qgis.org/projects/photo2shape&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Please report bugs at &lt;a href=&quot;http://hub.qgis.org/projects/photo2shape/issues&quot;&gt;bugtracker&lt;/a&gt;.&lt;/p&gt;</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gui/aboutdialog.py" line="77"/>
-        <source>&lt;p&gt;Create point shapefile from a set of geotagged photos. Inspired by ImagesToShape plugin from Tim Sutton.&lt;/p&gt;&lt;p&gt;NOTE: plugin requires 3rd party Python package exifread &gt;= 2.0.0&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Developers&lt;/strong&gt;: Alexander Bruy&lt;/p&gt;&lt;p&gt;&lt;strong&gt;Homepage&lt;/strong&gt;: &lt;a href=&quot;http://hub.qgis.org/projects/photo2shape&quot;&gt;http://hub.qgis.org/projects/photo2shape&lt;/a&gt;&lt;/p&gt;&lt;p&gt;Please report bugs at &lt;a href=&quot;http://hub.qgis.org/projects/photo2shape/issues&quot;&gt;bugtracker&lt;/a&gt;.&lt;/p&gt;</source>
+        <location filename="gui/aboutdialog.py" line="56"/>
+        <source>Version: {}</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
@@ -145,29 +145,12 @@ Le Plugin ne pourra pas être chargé.</translation>
     <message>
         <location filename="photo2shape_plugin.py" line="97"/>
         <source>Photo2Shape</source>
-        <translation type="unfinished">Photo2Shape</translation>
-    </message>
-    <message>
-        <location filename="photo2shape_plugin.py" line="67"/>
-        <source>QGIS %s detected.
-This version of Photo2Shape requires at least QGIS 2.0.
-Plugin will not be enabled.</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="photo2shape_plugin.py" line="76"/>
-        <source>Create a point shapefile from geotagged images</source>
-        <translation type="unfinished"></translation>
-    </message>
-    <message>
-        <location filename="photo2shape_plugin.py" line="79"/>
-        <source>About Photo2Shape...</source>
-        <translation type="unfinished"></translation>
+        <translation type="obsolete">Photo2Shape</translation>
     </message>
     <message>
         <location filename="photo2shape_plugin.py" line="82"/>
         <source>About Photo2Shape</source>
-        <translation type="unfinished">à propos</translation>
+        <translation type="obsolete">à propos</translation>
     </message>
 </context>
 <context>
@@ -283,76 +266,115 @@ Please enter a valid filename and try again.</source>
 Merci d&apos;entrer un nom valide et de réessayer.</translation>
     </message>
     <message>
-        <location filename="gui/photo2shapedialog.py" line="96"/>
+        <location filename="gui/photo2shapedialog.py" line="98"/>
         <source>ESRI Shapefiles (*.shp *.SHP)</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gui/photo2shapedialog.py" line="85"/>
+        <location filename="gui/photo2shapedialog.py" line="87"/>
         <source>Select directory</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gui/photo2shapedialog.py" line="99"/>
+        <location filename="gui/photo2shapedialog.py" line="101"/>
         <source>Save file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gui/photo2shapedialog.py" line="125"/>
+        <location filename="gui/photo2shapedialog.py" line="127"/>
         <source>Path not set</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gui/photo2shapedialog.py" line="125"/>
+        <location filename="gui/photo2shapedialog.py" line="127"/>
         <source>Path to photos is not set. Please specify directory with photos and try again.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gui/photo2shapedialog.py" line="133"/>
+        <location filename="gui/photo2shapedialog.py" line="135"/>
         <source>Output file is not set</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gui/photo2shapedialog.py" line="133"/>
+        <location filename="gui/photo2shapedialog.py" line="135"/>
         <source>Output file name is missing. Please specify correct output file and try again.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gui/photo2shapedialog.py" line="160"/>
+        <location filename="gui/photo2shapedialog.py" line="163"/>
         <source>Import completed</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="gui/photo2shapedialog.py" line="174"/>
+        <location filename="gui/photo2shapedialog.py" line="180"/>
         <source>Cannot load output shapefile</source>
         <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/photo2shapedialog.py" line="163"/>
+        <source>Shapefile from photos sucessfully created</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="gui/photo2shapedialog.py" line="180"/>
+        <source>No output</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Photo2ShapePlugin</name>
+    <message>
+        <location filename="photo2shape_plugin.py" line="66"/>
+        <source>QGIS {} detected.
+This version of Photo2Shape requires at least QGIS 2.0. Plugin will not be enabled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape_plugin.py" line="99"/>
+        <source>Photo2Shape</source>
+        <translation type="unfinished">Photo2Shape</translation>
+    </message>
+    <message>
+        <location filename="photo2shape_plugin.py" line="76"/>
+        <source>Create a point shapefile from geotagged images</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape_plugin.py" line="80"/>
+        <source>About Photo2Shape...</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="photo2shape_plugin.py" line="84"/>
+        <source>About Photo2Shape</source>
+        <translation type="unfinished">à propos</translation>
     </message>
 </context>
 <context>
     <name>PhotoImporter</name>
     <message>
-        <location filename="photoimporter.py" line="75"/>
+        <location filename="photoimporter.py" line="77"/>
         <source>Unable to open or create layer.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoimporter.py" line="89"/>
+        <location filename="photoimporter.py" line="91"/>
         <source>No images found in directory.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoimporter.py" line="101"/>
-        <source>Skipping file %s: there are no GPS tags in it.</source>
+        <location filename="photoimporter.py" line="66"/>
+        <source>Found exifread {}, but plugin requires exifread 1.x or &gt;= 2.0.1.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoimporter.py" line="109"/>
-        <source>Skipping file %s: there are no GPS fix data.</source>
+        <location filename="photoimporter.py" line="103"/>
+        <source>Skipping file {}: there are no GPS tags in it.</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="photoimporter.py" line="64"/>
-        <source>Found exifread %s, but plugin requires exifread 1.x or &gt;= 2.0.1.</source>
+        <location filename="photoimporter.py" line="112"/>
+        <source>Skipping file {}: there are no GPS fix data.</source>
         <translation type="unfinished"></translation>
     </message>
 </context>
