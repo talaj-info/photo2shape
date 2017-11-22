5a08334ef68be0215ddc50c0124549bb11121563
Szieberth Ádám <sziebadam@gmail.com>
2017-11-22 13:48:55 +0100

    del make.bat

--- make.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-
-rem Make sure you added Program Files\QGIS xxx\bin to your PATH
-
-call pyuic4 -o "ui\ui_aboutdialogbase.py" "ui\aboutdialogbase.ui"
-
-call pyuic4 -o "ui\ui_photo2shapedialogbase.py" "ui\photo2shapedialogbase.ui"
-
-call pyrcc4 -o "resources_rc.py" "resources.qrc"
